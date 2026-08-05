73fdc174cf9874f65e790f2d8be8564ed19970e8
jeanlouis22 <guejeanlouis5@gmail.com>
2026-08-05 18:33:23 +0000

    Add touch controls for player movement and shooting

--- game.js
+++ game.js
@@ -105,3 +105,50 @@ setInterval(function(){
     }
 
 },50);
+// Commandes téléphone
+
+document.getElementById("droite").addEventListener("touchstart", function(){
+    position += 20;
+
+    if(position > 760){
+        position = 760;
+    }
+
+    joueur.style.left = position + "px";
+});
+
+
+document.getElementById("gauche").addEventListener("touchstart", function(){
+    position -= 20;
+
+    if(position < 0){
+        position = 0;
+    }
+
+    joueur.style.left = position + "px";
+});
+
+
+document.getElementById("tirer").addEventListener("touchstart", function(){
+
+    let balle = document.createElement("div");
+    balle.className = "balle";
+
+    balle.style.left = position + 50 + "px";
+    balle.style.bottom = "50px";
+
+    document.getElementById("jeu").appendChild(balle);
+
+
+    let mouvement = setInterval(function(){
+
+        balle.style.left = balle.offsetLeft + 10 + "px";
+
+        if(balle.offsetLeft > 800){
+            balle.remove();
+            clearInterval(mouvement);
+        }
+
+    },50);
+
+});
